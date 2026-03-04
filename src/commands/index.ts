2e16c284e35a7d28c03c0d00a8fe86357e2f2813
revett <2796074+revett@users.noreply.github.com>
2026-03-04 21:17:00 +0000

    New format command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,13 @@
 import type { Command } from "../types/command";
 import { apply } from "./apply";
+import { format } from "./format";
 import { generate } from "./generate";
 import { init } from "./init";
 import { reminder } from "./reminder";
 
 export const commands: ReadonlyArray<Command> = [
   apply,
+  format,
   generate,
   init,
   reminder,
